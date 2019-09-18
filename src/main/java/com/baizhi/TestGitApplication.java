7e6394f61c0b9a5d0594a4d2888863abb692b52e
liuwenqiang111 <53990849+liuwenqiang111@users.noreply.github.com>
2019-09-18 17:13:34 +0800

    Update TestGitApplication.java

--- src/main/java/com/baizhi/TestGitApplication.java
+++ src/main/java/com/baizhi/TestGitApplication.java
@@ -9,6 +9,7 @@ public class TestGitApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestGitApplication.class, args);
         System.out.println("2.0");
+        System.out.println("3.0");
     }
 
 
