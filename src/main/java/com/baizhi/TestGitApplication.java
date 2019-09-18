e82875687c32e9863746d6c7c377d4d1a119537e
lwq <852812238@qq.com>
2019-09-18 17:03:47 +0800

    test-git2.0

--- src/main/java/com/baizhi/TestGitApplication.java
+++ src/main/java/com/baizhi/TestGitApplication.java
@@ -8,6 +8,8 @@ public class TestGitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestGitApplication.class, args);
+        System.out.println("2.0");
     }
+
 
 }
